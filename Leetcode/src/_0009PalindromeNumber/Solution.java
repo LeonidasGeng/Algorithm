89e742b90f0c1fd4bd05c4414771641b086dea3f
Leonidas <leonidas.geng@outlook.com>
2022-12-09 23:15:37 -0500

    Update Solution.java

--- Leetcode/src/_0009PalindromeNumber/Solution.java
+++ Leetcode/src/_0009PalindromeNumber/Solution.java
@@ -3,7 +3,7 @@ package _0009PalindromeNumber;
 public class Solution {
     public boolean isPalindrome(int x) {
         //特殊情况1：0是回文数
-        if (x == 0) return true; 
+        if (x == 0) return true;
         //特殊情况2：负数不是回文数
         if (x < 0) return false;
         //特殊情况3：以0结尾的数都不是回文数
